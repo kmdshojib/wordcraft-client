08f371fb5cd3fd8e9c0f78dba2e2ac620c7201dc
kmdshojib <kmd.shojib@gmail.com>
2024-12-13 04:42:10 +0600

    vercel json has been added

--- src/routes/SecureRoute.tsx
+++ src/routes/SecureRoute.tsx
@@ -1,29 +1,29 @@
-import { ReactNode, FC } from "react";
-// import { useNavigate } from "react-router";
-// import { useAppContext } from "../hooks/useAppContext";
-// import { message } from "antd";
+import { ReactNode, FC, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { useAppContext } from "../hooks/useAppContext";
+import { message } from "antd";
 
 interface SecureRouteProps {
     children: ReactNode;
 }
 
 const SecureRoute: FC<SecureRouteProps> = ({ children }) => {
-    // const { user }: any = useAppContext();
-    // const navigate = useNavigate();
-    // const [isAuthorized, setIsAuthorized] = useState(false);
+    const { user }: any = useAppContext();
+    const navigate = useNavigate();
+    const [isAuthorized, setIsAuthorized] = useState(false);
 
-    // useEffect(() => {
-    //     if (!user) {
-    //         message.error("Please login to access this page.");
-    //         navigate("/", { replace: true });
-    //     } else {
-    //         setIsAuthorized(true);
-    //     }
-    // }, [user, navigate]);
+    useEffect(() => {
+        if (!user) {
+            message.error("Please login to access this page.");
+            navigate("/", { replace: true });
+        } else {
+            setIsAuthorized(true);
+        }
+    }, [user, navigate]);
 
-    // if (!isAuthorized) {
-    //     return null; // Or return a loading spinner if you prefer
-    // }
+    if (!isAuthorized) {
+        return null; // Or return a loading spinner if you prefer
+    }
 
     return <>{children}</>;
 };
